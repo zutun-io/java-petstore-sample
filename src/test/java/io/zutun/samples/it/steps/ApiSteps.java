8eac12f572b5aeba9d32d080c9eb6530ce016694
Juan Diego Crisanto <154277583+jd-crisanto@users.noreply.github.com>
2025-02-12 01:15:53 -0500

    Feat/integration test driven by ai (#2)
    * feat(Doc): Add prompt to generate .feature files
    
    * feat(Doc): Add prompt to generate .feature files
    
    * feat(Doc): Add prompt to generate .feature files

--- src/test/java/io/zutun/samples/it/steps/ApiSteps.java
+++ src/test/java/io/zutun/samples/it/steps/ApiSteps.java
@@ -1,13 +1,19 @@
 package io.zutun.samples.it.steps;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.es.Cuando;
+import io.cucumber.java.es.Dado;
 import io.cucumber.java.es.Entonces;
+import io.cucumber.java.es.Y;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static io.restassured.http.Method.GET;
+import static io.restassured.http.Method.POST;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.matchesPattern;
 
 @CucumberContextConfiguration
 @SpringBootTest(classes = {ApiClient.class})
@@ -37,4 +43,21 @@ public class ApiSteps {
         apiClient.performRequest(GET, "/actuator/health");
     }
 
+    @Dado("que se tienen los siguientes headers")
+    public void withHeaders(DataTable headers) {
+        headers.asMap(String.class, String.class).forEach(apiClient::withHeader);
+    }
+
+    @Cuando("Se registra una nueva mascota")
+    public void registerNewPet(JsonNode jsonPayload) {
+        apiClient.withJsonPayload(jsonPayload.toString());
+        apiClient.performRequest(POST, "/v1/pets");
+    }
+
+    @Y("El response debe contener un campo {string} con formato UUID")
+    public void fielShouldHaveFormatUUID(String field) {
+        apiClient.getResponse()
+                .then()
+                .body(field, matchesPattern("^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$"));
+    }
 }
---
+++ src/test/resources/api/features/001_pet_register.feature
@@ -0,0 +1,18 @@
+# language: es
+Característica: Registro de mascotas
+
+  Escenario: Registrar un nuevo gato
+
+  Escenario: Registrar un nuevo perro
+
+  Escenario: Registrar un gato con un nombre ya existente
+
+  Escenario: Registrar un nuevo perro con los campos requeridos
+
+  Escenario: Registrar un gato con todos los campos (requeridos y opcionales)
+
+  Escenario: Registrar una nueva mascota de una especie no válida
+
+  Escenario: Registrar una nueva mascota con un nombre muy largo
+
+  Escenario: Registrar una nueva mascota con una edad negativa
