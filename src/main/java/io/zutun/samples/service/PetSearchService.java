3f00b523bbbcd0d9112fa50b67252b3d35b157b8
Alexander Belisario <abelisario1@gmail.com>
2025-05-07 21:12:07 -0500

    Implementación de funcionalidades de búsqueda/consulta de mascotas

---
+++ src/main/java/io/zutun/samples/service/PetSearchService.java
@@ -0,0 +1,51 @@
+package io.zutun.samples.service;
+
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import io.zutun.samples.adapter.in.web.response.PaginatedResponse;
+import io.zutun.samples.domain.Pet;
+import io.zutun.samples.usecases.SearchPetUseCase;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PetSearchService implements SearchPetUseCase {
+
+    private final PetsRepository repository;
+
+    public Optional<Pet> findById(UUID id) {
+        return repository.findById(id);
+    }
+
+    public PaginatedResponse<Pet> findAll(int page, int pagesize) {
+        Pageable pageable = PageRequest.of(page, pagesize);
+        Page<Pet> result = repository.findAll(pageable);
+        return new PaginatedResponse<>(result.getContent(), page, pagesize, result.getTotalElements(), result.getTotalPages());
+    }
+
+    public PaginatedResponse<Pet> findByName(String name, int page, int pagesize) {
+        Pageable pageable = PageRequest.of(page, pagesize);
+        Page<Pet> result = repository.findByNameIgnoreCase(name, pageable);
+        return new PaginatedResponse<>(result.getContent(), page, pagesize, result.getTotalElements(), result.getTotalPages());
+    }
+
+    public PaginatedResponse<Pet> findByBirthDate(LocalDate birthDate, int page, int pagesize) {
+        Pageable pageable = PageRequest.of(page, pagesize);
+        Page<Pet> result = repository.findByBirthDate(birthDate, pageable);
+        return new PaginatedResponse<>(result.getContent(), page, pagesize, result.getTotalElements(), result.getTotalPages());
+    }
+    
+    public PaginatedResponse<Pet> findByNameAndBirthDate(String name, LocalDate birthDate, int page, int pagesize) {
+        Pageable pageable = PageRequest.of(page, pagesize);
+        Page<Pet> result = repository.findByNameIgnoreCaseAndBirthDate(name, birthDate, pageable);
+        return new PaginatedResponse<>(result.getContent(), page, pagesize, result.getTotalElements(), result.getTotalPages());
+    } 
+}
+
